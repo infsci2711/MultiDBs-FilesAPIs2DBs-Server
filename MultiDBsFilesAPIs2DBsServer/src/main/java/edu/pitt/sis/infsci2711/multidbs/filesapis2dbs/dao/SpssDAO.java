4b029c0e39ae6b21e0929572450928f9a6575feb
MooninSun <changliu1931@gmail.com>
2015-02-20 20:10:54 -0500

    table

--- MultiDBsFilesAPIs2DBsServer/src/main/java/edu/pitt/sis/infsci2711/multidbs/filesapis2dbs/dao/SpssDAO.java
+++ MultiDBsFilesAPIs2DBsServer/src/main/java/edu/pitt/sis/infsci2711/multidbs/filesapis2dbs/dao/SpssDAO.java
@@ -12,7 +12,7 @@ public static boolean createTable(ArrayList<String> t) throws SQLException, Exce
 		
 		try (Connection connection = JdbcUtil1.getConnection()) {
 			
-			String sql="create table "+t.get(0)+"( ";
+			String sql="create table if not exists"+t.get(0)+"( ";
 			for(int i=1;i<t.size();i++){
 				sql=sql+" "+t.get(i)+" ";
 				i++;
