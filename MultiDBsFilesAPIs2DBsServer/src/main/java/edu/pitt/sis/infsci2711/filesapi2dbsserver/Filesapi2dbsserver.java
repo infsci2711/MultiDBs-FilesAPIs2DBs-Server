cb2c8f8b12fbbaa45b94d7301ed7497687fd72c7
MooninSun <changliu1931@gmail.com>
2015-02-20 19:57:47 -0500

    upload

--- MultiDBsFilesAPIs2DBsServer/src/main/java/edu/pitt/sis/infsci2711/filesapi2dbsserver/Filesapi2dbsserver.java
+++ MultiDBsFilesAPIs2DBsServer/src/main/java/edu/pitt/sis/infsci2711/filesapi2dbsserver/Filesapi2dbsserver.java
@@ -9,7 +9,7 @@ import edu.pitt.sis.infsci2711.multidbs.filesapis2dbs.utils.FileReader1;
 public class Filesapi2dbsserver {
 
 	public static void main(String[] args) throws SQLException, Exception {
-		FileReader1 fileReader1 = new FileReader1("DatasetTest.sav");
+		FileReader1 fileReader1 = new FileReader1("upload_files/DatasetTest.sav");
 		ArrayList<String> t=fileReader1.readSPSSCreat();
 		SpssService s=new SpssService();
 		boolean f=s.createTable(t);
