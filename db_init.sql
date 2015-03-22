55338459a418dcac27d48321fa9624797761833e
jpotato0o <1546232650@qq.com>
2015-03-22 14:59:27 -0400

    sql

--- db_init.sql
+++ db_init.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE  IF NOT EXISTS `infsci2711_tutorial`
-USE `infsci2711_tutorial`;
+CREATE DATABASE  IF NOT EXISTS `infsci2711_filesapi2dbs`
+USE `infsci2711_filesapi2dbs`;
 
 
 CREATE USER 'dataverse'@'localhost' IDENTIFIED BY 'dataverse';
-grant all privileges on `infsci2711_tutorial`.* to 'dataverse'@'localhost';
+grant all privileges on `infsci2711_filesapi2dbs`.* to 'dataverse'@'localhost';
 
 CREATE USER 'dataverse'@'%' IDENTIFIED BY 'dataverse';
-grant all privileges on `infsci2711_tutorial`.* to 'dataverse'@'%';
+grant all privileges on `infsci2711_filesapi2dbs`.* to 'dataverse'@'%';
 
 FLUSH PRIVILEGES;
