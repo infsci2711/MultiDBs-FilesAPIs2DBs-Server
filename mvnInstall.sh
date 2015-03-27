8554d8b1c61c0618aebe296fb6b044eeb2a504ea
jpotato0o <1546232650@qq.com>
2015-03-27 17:27:09 -0400

    mvnInstall.sh

--- mvnInstall.sh
+++ mvnInstall.sh
@@ -14,6 +14,7 @@ cd MultiDBs-FilesAPIs2DBs-Server
 cd MultiDBsFilesAPIs2DBsServerAPI
 cd target
 cp multidbsfilesapis2dbsserverapi-0.1-SNAPSHOT.jar ../../../deployed/
+cd /opt/project/deployed
 kill -9 $(lsof -t -i:7654)
 java -jar multidbsfilesapis2dbsserverapi-0.1-SNAPSHOT.jar > log.out 2> error.log < /dev/null &
 
