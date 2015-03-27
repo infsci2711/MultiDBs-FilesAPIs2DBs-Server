453bf88823a6e0fc23c1be0d275662f117da05bf
wed26 <weidai@Weis-MacBook-Pro.local>
2015-03-27 16:52:46 -0400

    mvn install shell

---
+++ mvnInstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd /opt
+cd project
+cd MultiDBs-FilesAPIs2DBs-WebClient
+git pull
+cd ..
+cd MultiDBs-FilesAPIs2DBs-Server
+git pull
+mvn install
+cd /opt/project/deployed
+rm multidbsfilesapis2dbsserverapi-0.1-SNAPSHOT.jar
+cd /opt/project
+cd MultiDBs-FilesAPIs2DBs-Server
+cd MultiDBsFilesAPIs2DBsServerAPI
+cd target
+cp multidbsfilesapis2dbsserverapi-0.1-SNAPSHOT.jar ../../../deployed/
+kill -9 $(lsof -t -i:7654)
+java -jar multidbsfilesapis2dbsserverapi-0.1-SNAPSHOT.jar > log.out 2> error.log < /dev/null &
+
