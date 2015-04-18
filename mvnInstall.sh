d376590eb9f7ab5caf43c964e8ff89ac6fda3177
wed26 <weidai@Weis-MacBook-Pro.local>
2015-04-18 11:14:29 -0400

    maven utl

--- mvnInstall.sh
+++ mvnInstall.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd /opt/project/MultiDBs-Utils
+git pull
+mvn install
 cd /opt
 cd project
 cd MultiDBs-FilesAPIs2DBs-WebClient
@@ -14,7 +17,6 @@ cd MultiDBs-FilesAPIs2DBs-Server
 cd MultiDBsFilesAPIs2DBsServerAPI
 cd target
 cp multidbsfilesapis2dbsserverapi-0.1-SNAPSHOT.jar ../../../deployed/
-cd /opt/project/deployed
 kill -9 $(lsof -t -i:7654)
 cd /opt/project/deployed
 java -jar multidbsfilesapis2dbsserverapi-0.1-SNAPSHOT.jar > log.out 2> error.log < /dev/null &
