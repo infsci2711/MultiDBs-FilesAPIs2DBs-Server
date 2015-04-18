e7cf58a8ba664bd92776ba695be18f7848550cbf
MooninSun <changliu1931@gmail.com>
2015-04-18 16:38:59 -0400

    sql privileges

--- db_init.sql
+++ db_init.sql
@@ -1,11 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `infsci2711_filesapi2dbs`;
-USE `infsci2711_filesapi2dbs`;
 
 
 CREATE USER 'dataverse'@'localhost' IDENTIFIED BY 'dataverse';
-grant all privileges on `infsci2711_filesapi2dbs`.* to 'dataverse'@'localhost';
+grant all privileges on *.* to 'dataverse'@'localhost';
 
 CREATE USER 'dataverse'@'%' IDENTIFIED BY 'dataverse';
-grant all privileges on `infsci2711_filesapi2dbs`.* to 'dataverse'@'%';
+grant all privileges on *.* to 'dataverse'@'%';
 
 FLUSH PRIVILEGES;
