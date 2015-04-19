f9b353120f209156911ff943764608906d7c83de
Weichuan <Weichuan0903@gmail.com>
2015-04-19 15:06:42 -0400

    install change

--- mvnInstall.sh
+++ mvnInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /opt/project/MultiDBs-Utils
 git pull
-mvn install
+mvn clean install
 cd /opt
 cd project
 cd MultiDBs-FilesAPIs2DBs-WebClient
@@ -9,7 +9,7 @@ git pull
 cd ..
 cd MultiDBs-FilesAPIs2DBs-Server
 git pull
-mvn install
+mvn clean install
 cd /opt/project/deployed
 rm multidbsfilesapis2dbsserverapi-0.1-SNAPSHOT.jar
 cd /opt/project
@@ -19,8 +19,5 @@ cd target
 cp multidbsfilesapis2dbsserverapi-0.1-SNAPSHOT.jar ../../../deployed/
 kill -9 $(lsof -t -i:7654)
 cd /opt/project/deployed
-java -jar multidbsfilesapis2dbsserverapi-0.1-SNAPSHOT.jar /opt/project/MultiDBs-FilesAPIs2DBs-Server/MultiDBsFilesAPIs2DBsServerAPI/src/main/resources/config.properties > log.out 2> error.log < /dev/null &
+nohup java -jar multidbsfilesapis2dbsserverapi-0.1-SNAPSHOT.jar /opt/project/MultiDBs-FilesAPIs2DBs-Server/MultiDBsFilesAPIs2DBsServerAPI/src/main/resources/config.properties > log.out 2> error.log < /dev/null &
 mysql -u dataverse -pdataverse
-show databases;
-use infsci2711_filesapi2dbs;
-show tables;
