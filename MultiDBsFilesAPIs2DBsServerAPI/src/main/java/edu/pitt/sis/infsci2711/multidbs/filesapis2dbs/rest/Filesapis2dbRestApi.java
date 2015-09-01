5bb2d88901a8e364748c3af3cedd53645f221aa3
EvgenyKarataev <karataev.evgeny@gmail.com>
2015-08-31 21:13:56 -0400

    add some test spss data and several logs statement in code

--- MultiDBsFilesAPIs2DBsServerAPI/src/main/java/edu/pitt/sis/infsci2711/multidbs/filesapis2dbs/rest/Filesapis2dbRestApi.java
+++ MultiDBsFilesAPIs2DBsServerAPI/src/main/java/edu/pitt/sis/infsci2711/multidbs/filesapis2dbs/rest/Filesapis2dbRestApi.java
@@ -142,35 +142,47 @@ public class Filesapis2dbRestApi {
 			@FormDataParam("file") InputStream uploadedInputStream,
 			@FormDataParam("file") FormDataContentDisposition fileDetail) throws IOException{
 	 
-			String uploadedFileLocation = "upload"+ File.separatorChar + fileDetail.getFileName();
-			String fileName = fileDetail.getFileName().replace(".sav", "");
-			// save it
-			writeToFile(uploadedInputStream, uploadedFileLocation);
-			String output = "File uploaded to : " + uploadedFileLocation;
-			System.out.println(output);
-			create(uploadedFileLocation);  //create table and tuples
+			logger.info("In SPSSUpload");
+		
+			String output = "";
 			
 			try {
-				DatasourceViewModel datasourceViewModel = new DatasourceViewModel("MySQL", 
-						PropertiesManager.getInstance().getStringProperty("ip"), 
-						PropertiesManager.getInstance().getIntProperty("database"), 
-						"dataverse", "dataverse", fileName.toLowerCase(), fileName.toLowerCase(), fileName.toLowerCase());
-				
-				logger.info(String.format("IP: %s, Port: %s, DBType: %s, Username: %s, Password: %s, DBName: %s", 
-						datasourceViewModel.getIpAddress(), datasourceViewModel.getPort(),
-						datasourceViewModel.getDbType(),datasourceViewModel.getUsername(), datasourceViewModel.getPassword(),
-						datasourceViewModel.getDbName()));
-				
-				Response result2 = JerseyClientUtil.doPut(PropertiesManager.getInstance().getStringProperty("metastore.rest.base"), 
-						PropertiesManager.getInstance().getStringProperty("metastore.rest.addDatasource"), datasourceViewModel);
 			
+				String uploadedFileLocation = "upload"+ File.separatorChar + fileDetail.getFileName();
+				String fileName = fileDetail.getFileName().replace(".sav", "");
+				// save it
+				writeToFile(uploadedInputStream, uploadedFileLocation);
+				output = "File uploaded to : " + uploadedFileLocation;
+				System.out.println(output);
+				create(uploadedFileLocation);  //create table and tuples
+				
+				try {
+					DatasourceViewModel datasourceViewModel = new DatasourceViewModel("MySQL", 
+							PropertiesManager.getInstance().getStringProperty("ip"), 
+							PropertiesManager.getInstance().getIntProperty("database"), 
+							"dataverse", "dataverse", fileName.toLowerCase(), fileName.toLowerCase(), fileName.toLowerCase());
+					
+					logger.info(String.format("IP: %s, Port: %s, DBType: %s, Username: %s, Password: %s, DBName: %s", 
+							datasourceViewModel.getIpAddress(), datasourceViewModel.getPort(),
+							datasourceViewModel.getDbType(),datasourceViewModel.getUsername(), datasourceViewModel.getPassword(),
+							datasourceViewModel.getDbName()));
+					
+					Response result2 = JerseyClientUtil.doPut(PropertiesManager.getInstance().getStringProperty("metastore.rest.base"), 
+							PropertiesManager.getInstance().getStringProperty("metastore.rest.addDatasource"), datasourceViewModel);
+				
+				}
+				catch (Exception e) {
+					logger.error("Request to metastore failed:", e);
+				}
 			}
-			catch (Exception e) {
-				logger.error("Request to metastore failed:", e);
+			catch(Exception e) {
+				
+				logger.error("In SPSSUpload error", e);
+				
+				throw e;
 			}
 			
 			return Response.status(200).entity(output).build();
-	 
 		}
 	
 		private  void  create(String uploadedFileLocation) throws IOException{
